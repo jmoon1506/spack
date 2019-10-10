250b79cdc6d4c53fe8c3a5f9ea269fe259a98dfe
Harsh Bhatia <bhatia4@llnl.gov>
2019-10-10 10:50:16 -0700

    updates to maestro

--- var/spack/repos/builtin/packages/py-maestrowf/package.py
+++ var/spack/repos/builtin/packages/py-maestrowf/package.py
@@ -23,9 +23,11 @@ class PyMaestrowf(PythonPackage):
     version('1.1.0', '3c20bf36fbb85d14c3bfdb865944a409')
     version('1.0.1', '6838fc8bdc7ca0c1adbb6a0333f005b4')
 
-    depends_on('py-setuptools', type='build')
+    depends_on('py-setuptools', type=('build', 'run'))
     depends_on('py-pyyaml',     type=('build', 'run'))
     depends_on('py-six',        type=('build', 'run'))
-    depends_on('py-enum34',     type=('build', 'run'))
+    # depends_on('py-enum34',     type=('build', 'run'))
     depends_on('py-tabulate',   type=('build', 'run'), when='@1.1.0:')
+    depends_on('py-tabulate',   type=('build', 'run'), when='@flux-dev:')
     depends_on('py-filelock',   type=('build', 'run'), when='@1.1.0:')
+    depends_on('py-filelock',   type=('build', 'run'), when='@flux-dev:')
