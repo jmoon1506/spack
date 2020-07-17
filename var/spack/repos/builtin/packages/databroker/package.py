179bbae1d550d35dfb5811f82db04263a3f176b5
Harsh Bhatia <bhatia4@llnl.gov>
2020-07-17 10:50:02 -0700

    updates to dbr

--- var/spack/repos/builtin/packages/databroker/package.py
+++ var/spack/repos/builtin/packages/databroker/package.py
@@ -18,35 +18,25 @@ class Databroker(CMakePackage, PythonPackage):
     url      = 'https://github.com/IBM/data-broker/archive/0.6.1.tar.gz'
     #git      = "git@github.com:IBM/data-broker.git"
 
+    version('0.7.0', sha256='5460fa1c5c05ad25c759b2ee4cecee92980d4dde5bc7c5f6da9242806cf22bb8')
     version('0.6.1', sha256='2c7d6c6a269d4ae97aad4d770533e742f367da84758130c283733f25df83e535')
     version('0.6.0', sha256='5856209d965c923548ebb69119344f1fc596d4c0631121b230448cc91bac4290')
 
     variant('python', default=False, description='Build Python bindings')
-    variant('temp', default=False, description='temp for testiing') 
 
     depends_on('cmake@2.8:',      type='build')
     depends_on('redis@5.0.2:',    type='run')
-    #depends_on('redis@4.0.8:',    type=('build', 'run'))
-    #depends_on('ruby@2.2.9:',    type=('build', 'run'))
     depends_on('libevent@2.1.8', type=('build', 'run'))
 
     extends('python@3.7:',        when='+python')
-    #depends_on('py-pip',          when='+python', type='build')
     depends_on('py-setuptools',   when='+python', type=('build', 'run'))
-    #depends_on('libffi@3.2.1',    when='+python', type=('build', 'run'))
-    #depends_on('py-cffi@1.11.5',  when='+python', type=('build', 'run'))
-    #depends_on('py-enum34@1.1.6', when='+python', type=('build', 'run'))
 
     patch('fixes_in_v0.6.1.patch', when='@0.6.1')
+    patch('fixes_in_v0.7.0.patch', when='@0.7.0')
 
-    #parallel = False
     def cmake_args(self):
         args = []
         args.append('-DDEFAULT_BE=redis')
-        #args.append('-DCMAKE_BUILD_TYPE=Debug')   # v0.6.1 fails for RelWithDebInfo (spack's default)
-        #args.append('-DCMAKE_COLOR_MAKEFILE=ON')
-        #args.append('-DCMAKE_VERBOSE_MAKEFILE=ON')
-
         if '+python' in self.spec:
             args.append('-DPYDBR=1')
         return args
